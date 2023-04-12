3374475580fe217ae3646ecab1c0fc0a08985903
Adrianzctpa <nocommentaryadrian@gmail.com>
2023-04-12 13:56:55 -0300

    Initial commit

---
+++ src/routes/error-page.tsx
@@ -0,0 +1,29 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.data.message}</i>
+            </p>
+            </div>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.message}</i>
+            </p>
+            </div>
+        );
+    } else {
+        return <></>
+    }
+}
